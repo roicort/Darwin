a7df8dab5d847c854b84a314a105728ba92c6e9b
Rodrigo <rochmo@live.com>
2018-12-10 20:29:54 -0600

    Lalala

--- EE/ee.py
+++ EE/ee.py
@@ -3,6 +3,9 @@ import scipy as sc
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from scipy import linalg
 
 def F(X):
@@ -57,3 +60,16 @@ print("Mean = " +str(median))
 print("Std = " +str(std))
 print("Max Value = " + str(maxx))
 print("Min Value = "+ str(minn))
+
+Bounds = [-50,50]
+x1 = np.linspace(Bounds[0], Bounds[1])
+x2 = np.linspace(Bounds[0], Bounds[1])
+X1, X2 = np.meshgrid(x1,x2)
+F = F([X1,X2])
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X1,X2,F, cmap=cm.rainbow,linewidth=0, antialiased=True)
+plt.grid(True)
+
+plt.show()
